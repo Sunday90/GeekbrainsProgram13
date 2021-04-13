eb3ac68b66e49535036d1c6b084477bf427aeac7
Konstantin Plisov <3dnow@bk.ru>
2021-04-14 00:41:40 +0500

    Plisov work 13

---
+++ src/main/java/ru/geekbrains/MainClass.java
@@ -0,0 +1,31 @@
+package ru.geekbrains;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+
+public class MainClass {
+    public static final int CARS_COUNT = 4;
+    public static void main(String[] args) {
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        CyclicBarrier cb = new CyclicBarrier(CARS_COUNT);
+        final CountDownLatch cdl = new CountDownLatch(CARS_COUNT);
+        Semaphore smp = new Semaphore(CARS_COUNT);
+        Race race = new Race(new Road(60), new Tunnel(smp), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cb, cdl);
+        }
+        for (int i = 0; i < cars.length; i++) {
+            new Thread(cars[i]).start();
+        }
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        try {
+            cdl.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+    }
+
+}
